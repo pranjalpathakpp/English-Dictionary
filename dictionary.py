cae3284f339734e6d6819b0c09ed16524ad71a75
pranjalpathakpp <134314345+pranjalpathakpp@users.noreply.github.com>
2023-05-30 21:57:30 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+def meaning (w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        new = input("Did you mean %s instead. Type y is yes or n if no:" % get_close_matches(w, data.keys())[0])
+        new = new.lower()
+        if new == 'y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif new == 'n':
+            return "Sorry the word entered was not found in the data"
+        else:
+            return "Type y/n not anything else"
+    else:
+        return "The word entered is wrong, recheck and type again"
+    
+enter = input("Enter your word:")
+means = meaning(enter)
+print(means)   
